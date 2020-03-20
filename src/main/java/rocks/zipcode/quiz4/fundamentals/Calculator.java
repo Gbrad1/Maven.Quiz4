a7ae2231f6137ea1874bc568115c01a1ddfd8914
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-20 15:42:13 -0400

    saving progress

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -5,31 +5,39 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value * value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        Double[] squareRootsOfValues = new Double[value.length];
+        for (int i = 0; i < squareRootsOfValues.length; i++) {
+            squareRootsOfValues[i] = Math.sqrt(value[i]);
+        }
+        return squareRootsOfValues;
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        Double[] squaredValues = new Double[values.length];
+        for (int i = 0; i < squaredValues.length; i++) {
+            squaredValues[i] = values[i] * values[i];
+        }
+        return squaredValues;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor / dividend;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,27 +1,64 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        int middleIndex = string.length()/2;
+        return string.charAt(middleIndex);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        String toReturn = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i == str.length() / 2) {
+                String toAdd = str.substring(i, i+1).toUpperCase();
+                toReturn += toAdd;
+            } else
+                toReturn += str.charAt(i);
+        }
+        return toReturn;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        String toReturn = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i == str.length() / 2) {
+                String toAdd = str.substring(i, i+1).toLowerCase();
+                toReturn += toAdd;
+            } else
+                toReturn += str.charAt(i);
+        }
+        return toReturn;
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        char[] chars = str.toCharArray();
+        /*With a single for loop I was able to complete the "positive tests" but not the
+        negative ones. To get the negative, I needed to use a nested for loop but
+        then the positive failed. To fix this solution, I sorted the string as we only
+        need a boolean value.*/
+        Arrays.sort(chars);
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
@@ -29,6 +66,15 @@ public class StringUtils {
     }
 
     public static String invertCasing(String str) {
-        return null;
+        String toReturn = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 97 && str.charAt(i) <= 122) {
+                toReturn += str.substring(i, i + 1).toUpperCase();
+            } else if (str.charAt(i) >= 65 && str.charAt(i) <= 90) {
+                toReturn += str.substring(i, i + 1).toLowerCase();
+            }else
+                toReturn += str.substring(i, i + 1);
+        }
+        return toReturn;
     }
 }
