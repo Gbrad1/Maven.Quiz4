92ae673654e4e46bf2d5b8c4d39f70d134173c88
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-20 18:32:36 -0400

    saving

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,23 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+
+    Map<String, Integer> mapOfStrings;
+
     public WordCounter(String... strings) {
+         mapOfStrings = new HashMap<>();
+         for (String element : strings) {
+             if (mapOfStrings.containsKey(element)) {
+                 mapOfStrings.put(element, mapOfStrings.get(element) + 1);
+             } else
+             mapOfStrings.put(element, 1);
+         }
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        return mapOfStrings;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,36 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+
+    List<BankAccount> bankAccounts = new ArrayList<>();
+
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+        BankAccount toReturn = bankAccounts.get(indexNumber);
+        for (int i = 0; i < bankAccounts.size(); i++) {
+            if (i == indexNumber) {
+                bankAccounts.remove(i);
+            }
+        }
+        return toReturn;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        bankAccounts.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        for (BankAccount account : bankAccounts) {
+            if (account == bankAccount) {
+                return true;
+            }
+        }
+        return false;
+        //throw new UnsupportedOperationException("Method not yet implemented");
     }
 }
