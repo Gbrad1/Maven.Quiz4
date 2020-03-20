df2394f7ea06c8686164dd2f031fce751aa83991
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-20 16:15:13 -0400

    completed arrays portion

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,35 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        int middleIndex = values.length/2;
+        return values[middleIndex];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        String[] buffer = new String[values.length - 1];
+        int count = 0;
+        int middleIndex = values.length/2;
+        for (int i = 0; i < values.length; i++) {
+            if (i != middleIndex) {
+                buffer[count++] = values[i];
+            }
+        }
+        return buffer;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String[] buffer = new String[values.length - 1];
+        int count = 0;
+        int lastIndex = values.length - 1;
+        for (int i = 0; i < values.length; i++) {
+            if (i != lastIndex) {
+                buffer[count++] = values[i];
+            }
+        }
+        return buffer;
     }
 }
